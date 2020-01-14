ca301148b8f44eb7ac9d4222f1eb26287da8f401
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2020-01-14 14:20:03 -0800

    Finished unicafe step1 and step2

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -19,28 +19,17 @@
 
 // import React from 'react'
 import ReactDOM from 'react-dom'
-
 import React, {useState} from 'react'
 
 
-const History = (props) => {
-  if(props.allClicks.length === 0 ) {
-    return(
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
-  }
-
+const Text = ({title}) => {
   return(
     <div>
-      Button press history: {props.allClicks.join(' ')}
+      <h1>{title}</h1>
     </div>
   )
 }
 
-
-//Passing Event Handlers to Child Components
 const Button = ({onClick, text}) => {
   return(
     <button onClick={onClick}>
@@ -49,73 +38,93 @@ const Button = ({onClick, text}) => {
   )
 }
 
-const App = (props) => {
-//----------------------------------------
-  // const [clicks, setClicks] = useState({
-  //   left: 0,
-  //   right: 0
-  // })
-
-  // // const handleLeftClick = () => {
-  // //   const newClicks = {
-  // //     ...clicks,
-  // //     left: clicks.left + 1,
-  // //   }
-
-  // //   setClicks(newClicks)
-  // // }
-
-  // // const handleRightClick = () => {
-  // //   const newClicks = {
-  // //     ...clicks,
-  // //     right: clicks.right + 1
-  // //   }
-
-  // //   setClicks(newClicks)
-  // // }
-
-  // const handleLeftClick = () => {
-  //   setClicks({...clicks, left: clicks.left +1})
-  // }
-
-  // const handleRightClick = () => {
-  //   setClicks({...clicks, right: clicks.right + 1})
-  // }
-  
-//----------------------------------------
-
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  //Every click is stored into a separate piece of state called allClicks that is initialized as an empty array:
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left+1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right+1)
-  }
-
-  const hello = (who) => {
-    return () => {
-      console.log("Hello, ", who)
-    }
-  }
-  
+const StatsInfo = ({category, count}) => {
   return(
     <div>
+      {category} : {count}
+    </div>
+  )
+}
+
+const Total = ({total, title}) => {
+  return(
+    <div>
+      {title} : {total}
+    </div>
+  )
+}
+
+const Average = ({positive, negative, neutral, title}) => {
+  const total = positive + negative + neutral
+
+  if(total === 0)
+  {
+    return(
       <div>
-        {left}
-        <Button onClick={handleLeftClick} text='Left'/>
-        <Button onClick={handleRightClick} text='Right'/>
-        {right}
-        <History allClicks={allClicks}/>
+        {title} : 0
       </div>
-      <Button onClick={hello("Andres")} text="Greeting" />
-      <Button onClick={hello("New User")} text="Greeting" />
+    )
+  }
+
+  const trueScore = positive - negative
+
+  const average = trueScore/total
+
+  return(
+    <div>
+      {title} : {average}
+    </div>
+  )
+}
+
+const PositiveStats = ({title, positive, negative, neutral}) => {
+  const total = positive + negative + neutral
+
+  if(total === 0)
+  {
+    return(
+      <div>
+        {title} : 0
+      </div>
+    )
+  }
+
+  const stats = (positive / total) * 100
+
+  return(
+    <div>
+      {title} : {stats} %
+    </div>
+  )
+}
+
+const App = (props) => {
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+
+  const title = "Give Feedback"
+  const stats = "Statistics"
+
+  const countGood = (data) => () => setGood(data)  
+  const countBad = (data) => () => setBad(data)
+  const countNeutral = (data) => () => setNeutral(data)
+
+  return(
+    <div>
+      <Text title={title} />
+      <Button onClick={countGood(good + 1)} text="Good" />
+      <Button onClick={countNeutral(neutral +1)} text="Neutral" />
+      <Button onClick={countBad(bad +1)} text="Bad" />
+      <Text title={stats} />
+      <StatsInfo category="Good" count={good} />
+      <StatsInfo category="Neutral" count={neutral} />
+      <StatsInfo category="Bad" count={bad} />
+
+      <Total title="All" total={good+neutral+bad} />
+      <Average title="Average" positive={good} negative={bad} neutral={neutral} />
+      <PositiveStats title="Positive" positive={good} negative={bad} neutral={neutral} />
+      
     </div>
   )
   }
