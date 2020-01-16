5c5acb3d2f807f15430507e552cdec05de0f2259
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2020-01-16 14:57:20 -0800

    Finished lecture

--- part2/part2/src/App.js
+++ part2/part2/src/App.js
@@ -1,64 +1,54 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-import React from 'react'
+import React, {useState} from 'react'
 import Note from './components/Note'
 
 
 //Destructuring assignment of props
-const App =({notes}) => {
-    // const {notes} = props
+const App =(props) => {
+    
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState('A new note...')
+    const [showAll, setShowAll] = useState(true)
 
-    // const rows = () => notes.map(note => 
-    //     <li key={note.id}> 
-    //         {note.content}
-    //     </li>
-    // )
+    const addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: notes.length + 1,
+        }
 
-    //or, however, not recommended
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+    }
 
-    // const rows = () => notes.map( (note,i) =>
-    //     <li key={i}>
-    //         {note.content}
-    //     </li>
-    // )
+    const handleNoteChange = (event) => {
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
 
-    const rows = () => notes.map( (note,i) =>
-        <Note key={i} note={note} />
+    //Since we have useState to true, it will always display all notes, but if we set it to false, it will start filtering based on important notes.
+    const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+    const rows = () => notesToShow.map( note =>
+        <Note key={note.id} note={note} />
     )
-
-    const result = notes.map( note => note.content)
-
-    console.log(result)
 
     return(
         <div>
             <h1> Notes </h1>
+            <div>
+                <button onClick={() => setShowAll(!showAll)} >
+                    show {showAll ? 'Important' : "All"}
+                </button>
+            </div>
             <ul>
                 {rows()}
             </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange}/>
+                <button type='submit'>Save</button>
+            </form>
         </div>
     )
 }
