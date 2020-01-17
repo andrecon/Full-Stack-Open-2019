a9ee610a13ce3faed3f9656a77ec2a6018ca7ded
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2020-01-17 14:55:48 -0800

    Finished lecture working on phonebook step 6

--- part2/part2/src/App.js
+++ part2/part2/src/App.js
@@ -1,13 +1,26 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Note from './components/Note'
+import axios from 'axios'
+
 
 
 //Destructuring assignment of props
 const App =(props) => {
     
-    const [notes, setNotes] = useState(props.notes)
+    const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('A new note...')
     const [showAll, setShowAll] = useState(true)
+
+
+    useEffect( () => {
+        console.log("effect")
+        axios.get('http://localhost:3001/notes').then(response => {
+            console.log("promise fullfilled")
+            setNotes(response.data)
+        })
+    }, [] )
+
+    console.log('render', notes.length, 'notes')
 
     const addNote = (event) => {
         event.preventDefault()
